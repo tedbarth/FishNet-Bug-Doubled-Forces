de385986668f51dad32f1c30fdb09614d6a93ff3
barth <sebastian.barth@disy.net>
2023-02-26 11:54:57 +0100

    feat: Removed movement limiting

--- Assets/Scripts/Acceleration.cs
+++ Assets/Scripts/Acceleration.cs
@@ -6,8 +6,6 @@ public class Acceleration : TickedNetworkBehaviour {
   [SerializeField] private float linearAccelerationForce;
   [SerializeField] private float angularAccelerationForce;
 
-  public float maxLinearVelocity = 200; // Units per second
-  public float maxAngularVelocity = 100; // Degrees per second
   public float linearAcceleration = 100f;
   public float angularAcceleration = 1000f;
 
@@ -27,11 +25,6 @@ public class Acceleration : TickedNetworkBehaviour {
       Debug.Log($"Acc Rotation: {angularAccelerationForce}");
       _body.AddTorque(angularAccelerationForce);
     }
-
-    _body.velocity = Vector3.ClampMagnitude(_body.velocity, maxLinearVelocity);
-    _body.angularVelocity = Math.Clamp(Math.Abs(_body.angularVelocity), 0, maxAngularVelocity)
-                            * Math.Sign(_body.angularVelocity);
-    _body.rotation = Utilities.limitToAmplitudeOf360(_body.rotation);
   }
 
   public void SetInput(float verticalInput, float horizontalInput) {
